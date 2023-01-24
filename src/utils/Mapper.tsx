42a206013cdcbbcdbb488cb3d6024fa005acc942
erwanparms <parmentier.erwan@gmail.com>
2023-01-24 17:37:22 +0100

    add Table of pizzas
    create assets to store img
    add mainScreen components
    add request to api/pizzas
    create common components style

---
+++ src/utils/Mapper.tsx
@@ -0,0 +1,9 @@
+import Montagnarde from '../assets/Montagnarde.jpg';
+import fromages from '../assets/fromages.jpg';
+import margherite from '../assets/margherite.jpeg';
+
+export const imgMap: Map<number, string> = new Map([
+    [1, fromages],
+    [2, margherite],
+    [3, Montagnarde],
+  ]);
